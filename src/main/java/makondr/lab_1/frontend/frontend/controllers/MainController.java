3c67d82fd101ddab88ec17734947f72af799174b
Matvey Kondratev <kondratev.matvei@yandex.ru>
2024-09-21 18:01:59 +0300

    Finished v1.0

--- src/main/java/makondr/lab_1/frontend/frontend/controllers/MainController.java
+++ src/main/java/makondr/lab_1/frontend/frontend/controllers/MainController.java
@@ -1,14 +1,18 @@
 package makondr.lab_1.frontend.frontend.controllers;
 
-import org.apache.logging.log4j.message.Message;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.HashMap;
 import java.util.Map;
 
 @Controller
@@ -20,24 +24,39 @@ public class MainController {
         return "index";
     }
 
-    @PostMapping
-    public String sendMessage(@RequestBody String message) {
+    @PostMapping()
+    public String sendMessage(@RequestParam String message) {
 
         getHttpRequest(message);
-        return "index";
+        return "redirect:/";
     }
 
     private static void getHttpRequest(String message) {
+
+        System.out.println(message);
+
         HttpClient client = HttpClient.newHttpClient();
 
         HttpRequest request = HttpRequest.newBuilder(URI.create("http://localhost:8081/api/v1"))
                 .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(message))
+                .POST(HttpRequest.BodyPublishers.ofString(convertToJson(message)))
                 .build();
 
         client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
                 .thenApply(HttpResponse::body)
                 .thenAccept(System.out::println)
                 .join();
+    }
+
+    private static String convertToJson(String message) {
+        Map<String, String>  body = new HashMap<>(){{put("message", message);}};
+
+
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.writeValueAsString(body);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
